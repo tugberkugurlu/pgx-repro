8720ea7d4767dd092829e8124d74d164ee5ce7e2
Tugberk Ugurlu <tugberk@outlook.com>
2020-11-21 16:40:41 +0000

    add logger
    output
    
    ```
    db_1   | 2020-11-21 16:38:49.933 UTC [1] LOG:  listening on IPv4 address "0.0.0.0", port 5432
    db_1   | 2020-11-21 16:38:49.933 UTC [1] LOG:  listening on IPv6 address "::", port 5432
    db_1   | 2020-11-21 16:38:49.938 UTC [1] LOG:  listening on Unix socket "/var/run/postgresql/.s.PGSQL.5432"
    app_1  | wait-for-it.sh: waiting 15 seconds for db:5432
    db_1   | 2020-11-21 16:38:49.953 UTC [24] LOG:  database system was interrupted; last known up at 2020-11-21 16:29:33 UTC
    db_1   | 2020-11-21 16:38:50.297 UTC [24] LOG:  database system was not properly shut down; automatic recovery in progress
    app_1  | wait-for-it.sh: db:5432 is available after 0 seconds
    db_1   | 2020-11-21 16:38:50.300 UTC [24] LOG:  redo starts at 0/1653910
    db_1   | 2020-11-21 16:38:50.300 UTC [24] LOG:  invalid record length at 0/1653948: wanted 24, got 0
    db_1   | 2020-11-21 16:38:50.300 UTC [24] LOG:  redo done at 0/1653910
    db_1   | 2020-11-21 16:38:50.318 UTC [1] LOG:  database system is ready to accept connections
    app_1  | 2020/11/21 16:38:50 info: [1] Dialing PostgreSQL server (map[host:db])
    db_1   | 2020-11-21 16:38:50.853 UTC [31] LOG:  incomplete startup packet
    app_1  | 2020/11/21 16:38:50 info: [3] Query (map[args:[] pid:33 rowCount:1 sql:select 1 as v0, 2 as v1; time:812.1µs])
    app_1  | hello there!!!! 1 2
    pgx-repro_app_1 exited with code 0
    ```

--- app/main.go
+++ app/main.go
@@ -3,27 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-
 	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/sirupsen/logrus"
+	"log"
 )
 
 func main() {
 	pgxURL := "postgresql://postgres-dev:s3cr3tp4ssw0rd@db:5432/dev"
-	conn, err := pgx.Connect(context.Background(), pgxURL)
+	dbConfig, err := pgxpool.ParseConfig(pgxURL)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Unable to parse config: %v\n", err)
 	}
-	defer conn.Close(context.Background())
+
+	dbConfig.ConnConfig.Logger = &pgxLogger{}
+	dbConfig.ConnConfig.LogLevel = pgx.LogLevelTrace
+
+	ctx1 := context.WithValue(context.Background(), "ctx_name", "1")
+	pool, err := pgxpool.ConnectConfig(ctx1, dbConfig)
+	if err != nil {
+		log.Fatalf("Unable to create pool: %v\n", err)
+	}
+	defer pool.Close()
+
+	ctx2 := context.WithValue(context.Background(), "ctx_name", "2")
+	conn, err := pool.Acquire(ctx2)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Release()
 
 	var v0 int64
 	var v1 int64
-	err = conn.QueryRow(context.Background(), "select 1 as v0, 2 as v1;").Scan(&v0, &v1)
+	ctx3 := context.WithValue(context.Background(), "ctx_name", "3")
+	err = conn.QueryRow(ctx3, "select 1 as v0, 2 as v1;").Scan(&v0, &v1)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("QueryRow failed: %v\n", err)
 	}
 
 	fmt.Println("hello there!!!!", v0, v1)
+}
+
+type pgxLogger struct {
+}
+
+func (l *pgxLogger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]interface{}) {
+	log.Printf("%s: [%v] %s (%v)\n", level.String(), ctx.Value("ctx_name"), msg, data)
+}
+
+func initPgxLogger(logger *logrus.Entry) pgx.Logger {
+	return &pgxLogger{
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tugberkugurlu/pgx-repro
 
 go 1.14
 
-require github.com/jackc/pgx/v4 v4.6.0
+require (
+	github.com/jackc/pgx/v4 v4.6.0
+	github.com/sirupsen/logrus v1.4.2
+)
